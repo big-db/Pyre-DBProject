158263ade9700534916bf8e1d4bda38c42b6f2fe
Caleb Foo <kaleb.is.caleb@gmail.com>
2020-10-26 19:50:11 +0800

    setup of redux store in react + minor UI changes

--- src/components/SignInModal/SignInModal.js
+++ src/components/SignInModal/SignInModal.js
@@ -1,14 +1,32 @@
 import React,{Component} from 'react';
 import './SignInModal.css'
 class SignInModal extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            username: "",
+            password: "",
+        }
+    }
+    validateForm(){
+        return this.st
+    }
+
+    updateUserInput(input){
+        this.setState({username: input});
+    }
+
+    updatePWInput(input){
+        this.setState({password: input});
+    }
     render(){
         return <div className="signInModal">
-            <button className="closeButton" onClick={this.props.clickHandler}>close</button>
-            <div className="signInDetails">
-                <input placeholder="Username"></input>
-                <input placeholder="Password"></input>
-                <a href="/home">Sign In</a>
-            </div>
+            <button className="closeButton" onClick={this.props.clickHandler}>Close</button>
+            <form className="signInDetails">
+                <input onChange={e => this.updateUserInput(e.target.value)} type="text" placeholder="Username" value={this.state.username}></input>
+                <input onChange={e => this.updatePWInput(e.target.value)} type="password" placeholder="Password" value={this.state.password}></input>
+                <input type ="submit"/>
+            </form>
             
         </div>
     }
