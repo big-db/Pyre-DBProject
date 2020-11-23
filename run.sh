86afd23bcfd715174006ba12f42b8f566d82ee83
lowen <lowenhere@gmail.com>
2020-11-24 04:07:46 +0800

    quieten install scripts

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+echo "Loading Configs ..."
 node loadConfig.js
+
+echo "Building App ..."
 yarn build
 
 cp -r build /app
 
+echo "Starting ..."
 nginx
--- setup.sh
+++ setup.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
 # install dependencies
-apt-get update -y
-apt-get install curl -y
+echo "Installing dependencies ..."
+apt-get -qq update -y
+apt-get -qq install curl -y
 
-apt-get remove npm nodejs -y
+apt-get -qq remove npm nodejs -y
 
 curl -sL https://deb.nodesource.com/setup_15.x | bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 
-apt-get update -y
-apt-get install nodejs yarn nginx -y
+apt-get -qq update -y
+apt-get -qq install nodejs yarn nginx -y
 
 # docker hacks
 mv ./node_modules ./node_modules.tmp
 mv ./node_modules.tmp ./node_modules
 
 # install node dependencies
-yarn -y
+echo "Installing node dependencies ..."
+yarn --silent
 
 # configure nginx
+echo "Setting nginx configs ..."
 sed -i '1idaemon off;' /etc/nginx/nginx.conf
 cp nginx.conf /etc/nginx/sites-available/default
